1b82e01dc1422eb51285cb899c143c6d524bc5fc
Shriram-V <86782337+Shriram-V@users.noreply.github.com>
2021-11-12 20:10:17 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+score = 0;
+
+function q_a_1(){
+    score = score + 1;
+    console.log(score);
+}
+
+function q_b_2(){
+score = score + 1;
+console.log(score);
+}
+
+function q_a_3(){
+    score = score + 1;
+    console.log(score);
+}
+
+function q_a_4(){
+    score = score + 1;
+    console.log(score);
+}
+function submit(){
+
+document.getElementById("output").innerHTML = score;
+}
